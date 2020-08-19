46d569ae43f0eba2e48dbac5e7e7c17e4b3fdd48
parsa <parsak550z@gmail.com>
2020-08-19 12:34:48 +0430

    deleted the amber and red Colors from TextTheme.dart

--- lib/Themes/TextTheme.dart
+++ lib/Themes/TextTheme.dart
@@ -9,6 +9,3 @@ final kShopScreenFunkoPopTheme = GoogleFonts.nunito(
     color: Colors.white, fontSize: 20, fontWeight: FontWeight.w900);
 final kShopScreenFunkoPopPriceTheme = GoogleFonts.nunito(
     color: Colors.white, fontSize: 30, fontWeight: FontWeight.w900);
-
-final ktestTheme = Colors.red;
-final ktesttTheme = Colors.amber;
