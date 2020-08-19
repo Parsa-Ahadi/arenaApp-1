ce285f569ce5fdc14528a16b308e61ae24353664
parsa <parsak550z@gmail.com>
2020-08-19 12:21:26 +0430

    added a test final to the TextTheme.dart

--- lib/Themes/TextTheme.dart
+++ lib/Themes/TextTheme.dart
@@ -2,8 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-final kShopScreenLabelTextStyle = GoogleFonts.nunito(color: Colors.black, fontSize: 30);
+final kShopScreenLabelTextStyle =
+    GoogleFonts.nunito(color: Colors.black, fontSize: 30);
 
+final kShopScreenFunkoPopTheme = GoogleFonts.nunito(
+    color: Colors.white, fontSize: 20, fontWeight: FontWeight.w900);
+final kShopScreenFunkoPopPriceTheme = GoogleFonts.nunito(
+    color: Colors.white, fontSize: 30, fontWeight: FontWeight.w900);
 
-final kShopScreenFunkoPopTheme = GoogleFonts.nunito(color : Colors.white, fontSize : 20,fontWeight: FontWeight.w900);
-final kShopScreenFunkoPopPriceTheme = GoogleFonts.nunito(color : Colors.white, fontSize : 30,fontWeight: FontWeight.w900);
+final ktestTheme = Colors.red;
